09e67abfd0bd7201dd2cd2ab361f5b2d5a641abd
JustHm <taggma12@gmail.com>
2025-02-06 20:10:11 +0900

    Init: "프로젝트 초기 설정"
    collectionView compositional layout 테스트

---
+++ CollectionViewTest/ViewController/Views/HomeView.swift
@@ -0,0 +1,43 @@
+//
+//  HomeView.swift
+//  CollectionViewTest
+//
+//  Created by 안정흠 on 2/5/25.
+//
+
+import UIKit
+
+final class HomeView: UIView {
+    lazy var collectionView = UICollectionView(frame: .zero,
+                                               collectionViewLayout: createCompositionalLayout())
+    
+}
+
+extension HomeView {
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { [weak self] section, env in
+            self?.firstSection()
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.scrollDirection = .horizontal
+        
+        let layout2 = UICollectionViewCompositionalLayout(sectionProvider: { [weak self] section, env in
+            self?.firstSection()
+        }, configuration: config)
+        return layout
+    }
+    func firstSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                     subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        
+        
+        return section
+    }
+}
